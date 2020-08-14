3f7bf8edd5d246ec0f8b1ed5fcc6b3fd199d2013
CodeYard <devp@DESKTOP-ETQU0SO.localdomain>
2020-08-14 15:00:03 +0530

    Added buying functionality Currently does not deduct money

--- virtualresources/webapp/product_view.py
+++ virtualresources/webapp/product_view.py
@@ -4,9 +4,37 @@ from .models import Product
 from django.contrib.auth.decorators import login_required
 # Buy a product
 # Shows the page where checkout page with the confirmation and stuff
+
+
 @login_required(login_url="/signin")
 def buy_product(request):
-    return render(request, "checkout.html")
+    product_id = request.GET.get("id")
+    product = None
+    try:
+        product = Product.objects.get(id=product_id)
+    except Product.DoesNotExist:
+        print(f"Could not find a product with id = {product_id}")
+
+    if request.POST:
+        # TODO do the checks like if user has enough money and stuff
+        if True:
+            return redirect(f"/checkout?id={product_id}")
+
+    return render(request, "buy.html", {'product': product})
+
+ 
+@login_required(login_url='/signin')
+def checkout(request):
+    prod_id = request.GET.get("id")
+    product = None
+    try:
+        product =Product.objects.get(id = prod_id)
+        # Do the same user currency checks here too
+
+    except Product.DoesNotExist:
+        print(f"Could not find {prod_id}")
+
+    return render(request,"checkout.html",{'product':product})
 
 # Explore Products
 def explore_product(request):
@@ -16,6 +44,8 @@ def explore_product(request):
 
 # Sell a product
 # Make it login required
+
+
 @login_required(login_url="/signin")
 def sell_product(request):
     product_creation_form = CreateProductForm(request.POST or None)
@@ -24,14 +54,12 @@ def sell_product(request):
         if product_creation_form.is_valid():
             # product_creation_form
             print("form is valid")
-            product :Product= product_creation_form.save(commit = False)
+            product: Product = product_creation_form.save(commit=False)
             product.seller = request.user
             product.save()
-            
-            
 
     return render(
         request,
         "sell.html",
-        {'form':product_creation_form}
+        {'form': product_creation_form}
     )
